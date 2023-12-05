fb5b656383e1448e78713c97f73f07ac9e8971ab
Mauricio Portillo <106699864+Pohe1999@users.noreply.github.com>
2023-12-04 22:25:54 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Registro from './components/Registro'
+
+function App() {
+
+  return (
+    <div>
+      <Registro />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Registro.jsx
@@ -0,0 +1,22 @@
+
+
+const Registro = () => {
+
+    return(
+        <div>
+            <form action="submit">
+                <label>Escribe tu nombre: </label>
+                <input type="email" name="email" placeholder="Escribe tu correo aqui" />
+                <br />
+                <label>Numero de telefono:</label>
+                <input type="number" name="telefono" placeholder="Escribe tu numero aqui" />
+                <br />
+                <label>Municipio:</label>
+                <textarea name="municipio" cols="30" rows="10"></textarea>
+                <button>Enviar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Registro
